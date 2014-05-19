1071372ef41327fbd94a4f3052e360b9d2c14dba
Bernardo Farah <ber.farah@gmail.com>
2014-05-18 22:45:15 -0700

    Oops

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
     mysecrets = File.join(Rails.root, 'config', 'mysecrets.yml')
     YAML.load(File.open(mysecrets).each do |key, value|
       ENV[key.to_s] = value
-    end if File.exists(env_file))
+    end if File.exists?(env_file))
   end
   # Settings specified here will take precedence over those in config/application.rb.
 
